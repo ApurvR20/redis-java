6047084b2dd304493d478a360a1aa077001a7391
ApurvR20 <62154933+ApurvR20@users.noreply.github.com>
2025-07-22 19:01:25 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,44 +7,75 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+
+  public static class MyRunnable implements Runnable{
+    private Socket clientSocket;
+    public MyRunnable(Socket clientSocket){
+      this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run(){
+      OutputStream outputStream = null;
+      BufferedReader br = null;
+      try {
+        InputStream input = clientSocket.getInputStream();
+        br = new BufferedReader(new InputStreamReader(input));
+      } catch (Exception e) {
+       System.out.println(e); 
+      }
+      try {
+
+        if(br != null){
+        String ping;
+        outputStream = clientSocket.getOutputStream();
+
+        while((ping = br.readLine()) != null){
+
+          if(ping.equals("PING")){
+          System.out.println(ping);
+          outputStream.write("+PONG\r\n".getBytes());
+          outputStream.flush();}
+      }}
+      } catch (Exception e) {
+        System.out.println("IOException: " + e.getMessage());
+      } finally {
+        try {
+            
+            outputStream.close();
+          clientSocket.close();
+        } catch (Exception e) {
+          System.out.println("Failed to close client conn");
+        }        
+      }
+        
+    }
+  }
+
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment this block to pass the first stage
-       ServerSocket serverSocket = null;
-       Socket clientSocket = null;
-       int port = 6379;
-       try {
-         serverSocket = new ServerSocket(port);
-         // Since the tester restarts your program quite often, setting SO_REUSEADDR
-         // ensures that we don't run into 'Address already in use' errors
-         serverSocket.setReuseAddress(true);
-         // Wait for connection from client.
-         clientSocket = serverSocket.accept();
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
+    int port = 6379;
+    try {
+      serverSocket = new ServerSocket(port);
+      serverSocket.setReuseAddress(true);
+      while(true)
+      {
+        clientSocket = serverSocket.accept();
+        Thread thread = new Thread(new MyRunnable(clientSocket));
+        thread.start();
 
-         InputStream input = clientSocket.getInputStream();
-         OutputStream outputStream  = clientSocket.getOutputStream();
-         BufferedReader br = new BufferedReader(new InputStreamReader(input));
-         String ping;
-
-         while((ping = br.readLine()) != null){
-
-          if((ping.contains("PING"))){
-            outputStream.write("+PONG\r\n".getBytes());
-          }
-         }
-         
-        } catch (IOException e) {
-         System.out.println("IOException: " + e.getMessage());
-       } finally {
-         try {
-           if (clientSocket != null) {
-             clientSocket.close();
-           }
-         } catch (IOException e) {
-           System.out.println("IOException: " + e.getMessage());
-         }
-       }
+        // try {
+        //   // thread.join();
+        // } catch (Exception e) {
+        //   System.out.println(e);
+        // }
+      }
+    } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+    } 
   }
 }
