94d0bb24626dae6b5133daa6c9f884bd032f734e
ApurvR20 <62154933+ApurvR20@users.noreply.github.com>
2025-07-23 18:29:12 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.regex.*;
 
 public class Main {
 
@@ -14,28 +15,51 @@ public class Main {
       this.clientSocket = clientSocket;
     }
 
+    public String reqParser(String req){
+
+      String[] info = req.split("\r\n");
+      String res = "";
+      int l = 0;
+      Pattern pattern = Pattern.compile("(\\d+)$");
+      Matcher matcher = pattern.matcher(info[0]);
+      if(matcher.find()){
+        l = Integer.parseInt(matcher.group(l));
+      }
+
+      for(int i = 1; i < l; i++){
+        if(info[i].equalsIgnoreCase("echo")){
+          res += (info[i+1] + "\r\n" + info[i+2] + "\r\n");
+          i+=2;
+        }
+      }
+
+      return res;
+    }
+
     @Override
     public void run(){
       OutputStream outputStream = null;
       BufferedReader br = null;
+      String req,res;
       try {
         InputStream input = clientSocket.getInputStream();
         br = new BufferedReader(new InputStreamReader(input));
+        req = br.readLine();
       } catch (Exception e) {
        System.out.println(e); 
       }
       try {
 
         if(br != null){
-        String ping;
         outputStream = clientSocket.getOutputStream();
 
-        while((ping = br.readLine()) != null){
+        //i think the changes are supposed to be done here
+        while((req = br.readLine()) != null){
 
-          if(ping.equals("PING")){
-          System.out.println(ping);
-          outputStream.write("+PONG\r\n".getBytes());
-          outputStream.flush();}
+          //use parser here
+          res = reqParser(req);
+          outputStream.write(res.getBytes());
+          outputStream.flush();
       }}
       } catch (Exception e) {
         System.out.println("IOException: " + e.getMessage());
@@ -62,17 +86,13 @@ public class Main {
     try {
       serverSocket = new ServerSocket(port);
       serverSocket.setReuseAddress(true);
+
+      //why do i have this while(true)
       while(true)
       {
         clientSocket = serverSocket.accept();
         Thread thread = new Thread(new MyRunnable(clientSocket));
         thread.start();
-
-        // try {
-        //   // thread.join();
-        // } catch (Exception e) {
-        //   System.out.println(e);
-        // }
       }
     } catch (IOException e) {
         System.out.println("IOException: " + e.getMessage());
